c0dbaaa6f36e6063e03673f51f3125958a11b907
Jackson Lang <103084719+jacksonlang@users.noreply.github.com>
2022-04-13 19:54:45 -0400

    Iterates through games on date

--- SystemManager.py
+++ SystemManager.py
@@ -1,2 +1,56 @@
 import http.client
 import json
+from datetime import date
+
+today = date.today()
+
+s =today.strftime('%Y-%m-%d')
+
+#print(s)
+
+
+
+with open('BasketballTeams.json', 'r') as fp:
+    teamsjson = json.load(fp)
+
+teamLib = json.loads(teamsjson)
+
+#print(teamLib["response"][1]["id"])
+
+
+conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
+
+#headers = {
+#    'X-RapidAPI-Host': "api-nba-v1.p.rapidapi.com",
+#    'X-RapidAPI-Key': "b000ca23a4mshddcbd4524667073p1d4364jsnef1cddc8cc67"
+#    }
+
+#conn.request("GET", "/games?date="+s, headers=headers)
+
+#res = conn.getresponse()
+#data = res.read()
+
+#jsongames = data.decode("utf-8")
+
+
+
+#with open('Games.json', 'w') as f:
+#    json.dump(jsongames, f, ensure_ascii=False, indent=4)
+#
+with open('Games.json', 'r') as fp:
+    gamesjson = json.load(fp)
+
+gamesLib = json.loads(gamesjson)
+NBAGamesToday = len(gamesLib["response"])
+
+
+
+#print("Visitor: "+ teamLib["response"][gamesLib["response"][0]["teams"]["visitors"]["id"] - 1]["name"]
+#    + " |||| Home: " + teamLib["response"][gamesLib["response"][0]["teams"]["home"]["id"] - 1]["name"])
+
+i = 0
+
+while i <= NBAGamesToday - 1:
+    print("Visitor: "+ teamLib["response"][gamesLib["response"][i]["teams"]["visitors"]["id"] - 1]["name"]
+    + " |||| Home: " + teamLib["response"][gamesLib["response"][i]["teams"]["home"]["id"] - 1]["name"])
+    i += 1
